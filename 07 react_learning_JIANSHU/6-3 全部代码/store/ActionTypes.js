5ca0c781e1aa412bc5548f6b313ee1bcab80c6d4
RonnyJung <18856325009@163.com>
2019-09-28 15:29:21 +0800

    --

--- 07 react_learning_JIANSHU/6-3 全部代码/store/ActionTypes.js
+++
@@ -1,5 +0,0 @@
-export const CHANGE_INOUT_VALUE = 'change_input_value';
-export const ADD_TODO_ITEM = 'add_todo_item';
-export const DELETE_TODO_ITEM = 'delete_todo_item';
-export const GET_AJAX_DATA = 'get_ajax_data';
-
